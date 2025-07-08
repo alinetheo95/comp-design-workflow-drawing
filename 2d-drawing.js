70afe4df11f1a86d0e647bc1d9f3a506c5ed92a6
alinetheo95 <at4097@columbia.edu>
2025-07-08 12:50:15 -0400

    Update 2d-drawing.js

--- 2d-drawing.js
+++ 2d-drawing.js
@@ -11,38 +11,62 @@ var sketch1 = function(p) {
     canvas.parent('canvas-container-1');
   };
 
-  p.draw = function() {
-    p.background(250);
-    drawGrid();
-    drawPrimitives();
-  };
+let circles = [];
 
-  function drawGrid() {
-    p.stroke(200);
-    p.strokeWeight(1);
-    for (var x = 0; x <= p.width; x += gridSpacing) {
-      p.line(x, 0, x, p.height);
-    }
-    for (var y = 0; y <= p.height; y += gridSpacing) {
-      p.line(0, y, p.width, y);
-    }
+function setup() {
+  angleMode(DEGREES);
+
+  // Define circle positions and RGB color
+  circles = [
+    { x: 50, y: 100, color: [217, 137, 72] },
+    { x: 100, y: 300, color: [217, 137, 72] },
+    { x: 200, y: 80, color: [243, 110, 55] },
+    { x: 250, y: 220, color: [241, 95, 49] },
+    { x: 300, y: 340, color: [239, 66, 58] },
+    { x: 350, y: 180, color: [236, 44, 61] },
+    { x: 400, y: 70, color: [229, 35, 100] },
+    { x: 450, y: 250, color: [208, 28, 103] },
+    { x: 500, y: 350, color: [129, 97, 130] },
+    { x: 600, y: 120, color: [92, 64, 91] },
+    { x: 650, y: 300, color: [79, 47, 63] },
+  ];
+}
+
+function draw() {
+  background('#000000');
+  strokeWeight(2);
+
+  // First draw gradient lines
+  for (let i = 0; i < circles.length - 1; i++) {
+    drawGradientLine(circles[i], circles[i + 1], 50);
   }
 
-  function drawPrimitives() {
-    // Rectangle
-    p.fill(255, 100, 100);
-    p.rect(120, 80, 100, 60);
-    // Ellipse
-    p.fill(100, 180, 255);
-    p.ellipse(350, 200, 90, 90);
-    // Line
-    p.stroke(80, 200, 120);
-    p.strokeWeight(4);
-    p.line(500, 100, 700, 300);
-    // Triangle
-    p.noStroke();
-    p.fill(255, 220, 80);
-    p.triangle(600, 80, 750, 60, 700, 200);
+  // Then draw all circles
+  for (let circle of circles) {
+    drawCircle(circle.x, circle.y, circle.color);
+  }
+}
+
+function drawCircle(x, y, rgbArray) {
+  fill(...rgbArray);
+  noStroke();
+  circle(x, y, 70);
+}
+
+// Interpolated line segments to simulate gradient
+function drawGradientLine(c1, c2, steps) {
+  for (let i = 0; i < steps; i++) {
+    let t = i / (steps - 1);
+    let x = lerp(c1.x, c2.x, t);
+    let y = lerp(c1.y, c2.y, t);
+
+    let r = lerp(c1.color[0], c2.color[0], t);
+    let g = lerp(c1.color[1], c2.color[1], t);
+    let b = lerp(c1.color[2], c2.color[2], t);
+
+    stroke(r, g, b);
+    point(x, y);
+  }
   }
 };
 
